d5de569a970d97cbd4f34ace613ae25e324988f8
jins-coder <jinsonjosephkocheril@gmail.com>
2019-10-02 15:33:17 +0530

    Custom Color for Font Awesome Icons

--- storage/framework/views/0b0b9dddc1d32388f76f8ebd0b70a040fa8898a3.php
+++ storage/framework/views/0b0b9dddc1d32388f76f8ebd0b70a040fa8898a3.php
@@ -166,7 +166,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                          with font-awesome or any other icon font library -->
                     <li class="nav-item">
                         <router-link to="/dashboard" class="nav-link">
-                            <i class="nav-icon fas fa-tachometer-alt"></i>
+                            <i class="nav-icon fas fa-tachometer-alt pink"></i>
                             <p>
                               Dashboard
                                 <!-- <span class="right badge badge-danger">A</span> -->
@@ -176,7 +176,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                    
                     <li class="nav-item has-treeview ">
                         <a href="#" class="nav-link">
-                            <i class="nav-icon fas fa-cog"></i>
+                            <i class="nav-icon fas fa-cog green"></i>
                             <p>
                                 Management
                                 <i class="right fas fa-angle-left"></i>
@@ -199,7 +199,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                     </li>
                     <li class="nav-item">
                         <router-link to="/profile" class="nav-link">
-                            <i class="nav-icon fas fa-user"></i>
+                            <i class="nav-icon fas fa-user teal"></i>
                             <p>
                               Profile
                             
@@ -207,13 +207,15 @@ scratch. This page gets rid of all links and provides the needed markup only.
                         </router-link>
                     </li>
                     <li class="nav-item">
-                        <router-link to="/logout" class="nav-link">
-                            <i class="nav-icon fas fa-power-off"></i>
-                            <p>
-                              Logout
-                            
-                            </p>
-                        </router-link>
+                       <a class="nav-link" href="<?php echo e(route('logout')); ?>"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                     <i class="nav-icon fas fa-power-off red"></i><p><?php echo e(__('Logout')); ?></p>
+                                    </a>
+
+                          <form id="logout-form" action="<?php echo e(route('logout')); ?>" method="POST" style="display: none;">
+                                        <?php echo csrf_field(); ?>
+                         </form>
                     </li>
                 </ul>
             </nav>
